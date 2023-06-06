050d3560d33677c2f0edfd61f26b24e28d73e72a
PedroPB9 <pedropasqualb@gmail.com>
2023-06-06 10:17:15 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id": 1,
+    "photo": "https://coelhonews.com/_next/image?url=https%3A%2F%2Fimages.nintendolife.com%2F5750f69e77f7e%2Flarge.jpg&w=3840&q=75",
+    "title": "Developer Leaks Exclusive Infos On MK1",
+    "description": "The next Mortal Kombat game — titled Mortal Kombat 1, an entry that will bring the series into the past — is (officially) coming this year. Warner Bros. Games and longtime series developer NetherRealm Studios announced the next Mortal Kombat game on Thursday."
+  },
+  {
+    "id": 2,
+    "photo": "https://www.hindustantimes.com/ht-img/img/2023/04/07/1600x900/meme-1-5_1680834866502_1680834876780_1680834876780.jpeg",
+    "title": "Rockstar releases brand new features of GTA VI that will blow up your mind!",
+    "description": "According to leaks, GTA VI will likely feature multiple cities, including a return to Vice City, the iconic Miami-inspired location from Grand Theft Auto: Vice City. Reports suggest that players will also have the opportunity to explore a new South American-inspired city, expanding the game's scope and providing a diverse range of environments to explore."
+  }
+]
